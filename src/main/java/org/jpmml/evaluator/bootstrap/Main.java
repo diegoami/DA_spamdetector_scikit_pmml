8e9db794b6d4db45cc7c6d7b0cd2a54b83e4c75c
Diego Amicabile <diego.amicabile@gmail.com>
2019-07-08 19:58:22 +0200

    Working PMML model in JAVA

--- src/main/java/org/jpmml/evaluator/bootstrap/Main.java
+++ src/main/java/org/jpmml/evaluator/bootstrap/Main.java
@@ -18,21 +18,103 @@
  */
 package org.jpmml.evaluator.bootstrap;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
+import com.google.common.base.Splitter;
+import org.dmg.pmml.FieldName;
 import org.dmg.pmml.PMML;
-import org.jpmml.evaluator.Evaluator;
-import org.jpmml.evaluator.HasGroupFields;
-import org.jpmml.evaluator.HasOrderFields;
-import org.jpmml.evaluator.InputField;
-import org.jpmml.evaluator.ModelEvaluatorFactory;
-import org.jpmml.evaluator.OutputField;
-import org.jpmml.evaluator.TargetField;
+import org.jpmml.evaluator.*;
 import org.jpmml.model.PMMLUtil;
 
+
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+
+import com.google.common.cache.CacheBuilderSpec;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import org.dmg.pmml.FieldName;
+import org.dmg.pmml.PMML;
+import org.dmg.pmml.PMMLObject;
+;
+import org.jpmml.model.visitors.LocatorNullifier;
+import org.jpmml.model.visitors.MemoryMeasurer;
+
+
 public class Main {
+
+	private static final List<String> missingValues = Arrays.asList("N/A", "NA");
+
+
+	static
+	public CsvUtil.Table readTable(File file, String separator) throws IOException {
+
+		try(InputStream is = new FileInputStream(file)){
+			return CsvUtil.readTable(is, separator);
+		}
+	}
+
+
+	static
+	public java.util.function.Function<String, String> createCellParser(final Collection<String> missingValues){
+		java.util.function.Function<String, String> function = new Function<String, String>(){
+
+			@Override
+			public String apply(String string){
+
+				if(missingValues != null && missingValues.contains(string)){
+					return null;
+				}
+
+				// Remove leading and trailing quotation marks
+				string = stripQuotes(string, '\"');
+				string = stripQuotes(string, '\"');
+
+				// Standardize European-style decimal marks (',') to US-style decimal marks ('.')
+				if(string.indexOf(',') > -1){
+					String usString = string.replace(',', '.');
+
+					try {
+						Double.parseDouble(usString);
+
+						string = usString;
+					} catch(NumberFormatException nfe){
+						// Ignored
+					}
+				}
+
+				return string;
+			}
+
+			private String stripQuotes(String string, char quoteChar){
+
+				if(string.length() > 1 && ((string.charAt(0) == quoteChar) && (string.charAt(string.length() - 1) == quoteChar))){
+					return string.substring(1, string.length() - 1);
+				}
+
+				return string;
+			}
+		};
+
+		return function;
+	}
+
+	static private String stripQuotes(String string, char quoteChar){
+
+		if(string.length() > 1 && ((string.charAt(0) == quoteChar) && (string.charAt(string.length() - 1) == quoteChar))){
+			return string.substring(1, string.length() - 1);
+		}
+
+		return string;
+	}
 
 	static
 	public void main(String... args) throws Exception {
@@ -85,5 +167,55 @@ public class Main {
 		for(OutputField outputField : outputFields){
 			System.out.println("\t" + outputField);
 		}
+
+		File input = new File(args[1]);
+
+		CsvUtil.Table inputTable = readTable(input, ",");
+
+
+		InputStream is = new FileInputStream(input);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+		double accurate = 0;
+		double total = 0;
+		reader.readLine();
+		while(true) {
+			String line = reader.readLine();
+			if (line == null)
+				break;
+
+			String message = Main.stripQuotes(line.substring(0, line.length() - 2), '"');
+
+			String labelString = line.substring(line.length() - 1, line.length());
+			System.out.println(message+ ":"+ labelString);
+			try {
+				int label = Integer.parseInt(labelString );
+
+				Map<FieldName, String> evalMap = new HashMap<FieldName, String>();
+				evalMap.put(FieldName.create("x1"), message);
+				Map<FieldName, ?> resultMap = evaluator.evaluate(evalMap);
+				//System.out.println(message+ ":"+ label);
+				System.out.println(resultMap.keySet());
+				Object o = resultMap.get(FieldName.create("probability(1)"));
+				Double d = (Double)o;
+				System.out.println(d);
+				int isSpam = d > 0.5 ? 1 : 0;
+				System.out.println("Prediction: "+isSpam);
+				if (isSpam == label) {
+					accurate += 1;
+				}
+				total += 1;
+				System.out.println("Accuracy so far: "+(accurate/total));
+			} catch (NumberFormatException nfe) {
+
+				nfe.printStackTrace();
+			}
+
+
+
+		}
+
+		System.out.println("Accuracy : "+(accurate/total));
+
+
 	}
 }
